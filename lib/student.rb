f5a76d0b9b6984a2a62abe713ce5291001333faf
Tariq Bailey <tariqbailey627@gmail.com>
2020-01-13 23:53:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,14 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+attr_accessor :name, :grade
+attr_reader :id
+
+ def initialize(id = nil, name, grade)
+ @grade = grade
+ @name = nameend
+ end
+ 
+ 
 
 end
